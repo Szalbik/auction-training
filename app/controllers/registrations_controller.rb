bf774d178de5932ecdad212ddeaaedf6a2c38bb8
Damian Szalbierz <damian.szalbierz@netguru.com>
2020-06-18 10:54:29 +0200

    fix request error

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,5 +2,9 @@
 
 class RegistrationsController < Devise::RegistrationsController  
   respond_to :html, :json
-  clear_respond_to if request.format == 'json'
+  before_action :trigger_clear_respond_to, if: -> { request.format == 'json' }
+
+  def trigger_clear_respond_to
+    clear_respond_to
+  end
 end 
