7bbf9c3adbf380c79f16099bc654b9b7c4b8ddbd
Damian Szalbierz <damian.szalbierz@netguru.com>
2020-07-23 10:01:21 +0200

    Allow to update product only if all auctions ends

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,4 +4,12 @@ class Product < ApplicationRecord
   belongs_to :seller
   belongs_to :group, optional: true
   belongs_to :organization, optional: true
+
+  validate :there_cannot_be_open_auction, on: :update
+
+  def there_cannot_be_open_auction
+    if user_auctions.any? {|a| a.finished_at > Time.zone.now}
+      errors.add(:existing_auctions, "can't have open auction!")
+    end
+  end
 end
