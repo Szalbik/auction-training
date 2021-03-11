e8f1f281c753c00f89a69a76bb466b8ffa810055
Damian Szalbierz <damian.szalbierz@netguru.com>
2021-03-11 23:23:22 +0100

    revert jqery easing

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -4,6 +4,7 @@ const webpack = require('webpack')
 environment.plugins.append('Provide', new webpack.ProvidePlugin({
   $: 'jquery',
   jQuery: 'jquery',
+  'jquery.easing': 'jquery.easing',
   Popper: ['popper.js', 'default']
 }))
 
